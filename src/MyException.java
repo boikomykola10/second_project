a90af9af416417d8df0eb13f6ee9f63126ffc16b
boikomykola10 <81511227+boikomykola10@users.noreply.github.com>
2022-09-26 16:26:30 +0300

    Update MyException.java

--- src/MyException.java
+++ src/MyException.java
@@ -4,7 +4,7 @@ public class MyException extends Exception {
     public MyException(int x) {
         id = x;
     }
-
+    // comment from GitHub
     public void mssOfMyException() {
         System.err.println("My exception");
     }
